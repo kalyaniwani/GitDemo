95ff29ed9509d016d73037f3631210e525e4b3c4
kalyaniwani <refer2kalyani@gmail.com>
2022-12-24 17:19:42 +0530

    first comment:added Arraylist demo first

---
+++ src/com/git/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package com.git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+List<String> list= new ArrayList<String>();
+list.add("laptop");
+System.out.println(list);
+	}
+
+}
