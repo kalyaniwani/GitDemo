afa34f93e877d2a12911d108aa44c5dc3a080140
kalyaniwani <refer2kalyani@gmail.com>
2022-12-24 18:09:57 +0530

    second comment: added new LinkedList

---
+++ src/com/git/LinkedListDemo.java
@@ -0,0 +1,15 @@
+package com.git;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list= new LinkedList<String>();
+		list.add("Kalyani");
+		System.out.println(list);
+	}
+
+}
