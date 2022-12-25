2c100481453d1e142d268ac469bb4361f47cfa4d
kalyaniwani <refer2kalyani@gmail.com>
2022-12-25 12:19:51 +0530

    third commit:print numbers code added

---
+++ src/com/git/Print.java
@@ -0,0 +1,27 @@
+package com.git;
+
+public class Print {
+	
+	
+	public static  void print(int n) {
+		
+		if(n>0) {
+			print(n-1);
+			System.out.print(n+++" ");
+		}
+	}
+	
+	public static void print1(int n) {
+		
+		if(n>0) {
+			System.out.print(n+" ");
+			print1(n-1);
+		}
+	}
+	public static void main(String[] args) {
+		
+		print1(100);
+		
+	}
+
+}
